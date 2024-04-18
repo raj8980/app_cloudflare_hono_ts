f75c6d55b4a4f1f8535aa341e546079945bd6c3c
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-19 02:18:48 +0930

    Addedhono middleware.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async (c) => {
-  const body = await c.req.json();
-  console.log(body);
-  console.log(c.req.header("Authorization"));
-  console.log(c.req.query("param"));
+async function authMiddleWare(c:any,next:any){
+  if(c.req.header("Authorization")){
+    await next();
+  }else{
+    return c.text("You don't have access");
+  }
+}
+
+app.use(authMiddleWare)
+
+app.get('/', authMiddleWare,async (c) => {
+  // const body = await c.req.json();
+  // console.log(body);
+  // console.log(c.req.header("Authorization"));
+  // console.log(c.req.query("param"));
   return c.json({
     message:"Hi There"
   })
