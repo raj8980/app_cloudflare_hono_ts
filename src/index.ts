3df88212f1aad5313642301a741bd37bd2d80401
Raj Patel <rajpatel8980.rp42@gmail.com>
2024-04-19 02:01:48 +0930

    modified files to initiallize hono library

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  return c.json({
+    message:"Hi There"
+  })
+});
+
+
+
+export default app
